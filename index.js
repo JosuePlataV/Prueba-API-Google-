61f3a2fb6dac8689d583339547345431b6b23326
JosuePlataV <144969668+JosuePlataV@users.noreply.github.com>
2024-07-21 21:10:43 -0500

    ?

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 import { GoogleLogin } from 'react-google-login';
 let Container = document.querySelector(".g-signin2"),
 clientid = "164538105610-d0fclos8th1prrb4fem2vjllsj3cg49o.apps.googleusercontent.com",
-
+secret = "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"
 
 function init() {
     gapi.load('auth2', function() {
