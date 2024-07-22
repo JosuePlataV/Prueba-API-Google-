770517637c926fdd16369ef574d336d50688e12a
JosuePlataV <144969668+JosuePlataV@users.noreply.github.com>
2024-07-21 20:21:34 -0500

    q

--- README.md
+++
@@ -1,1 +0,0 @@
-# Prueba-API-Google-
---
+++ index.js
@@ -0,0 +1,9 @@
+let Container = document.querySelector("#Container"),
+url = "https://people.googleapis.com/$discovery/rest?version=v1",
+email = "https://www.googleapis.com/auth/userinfo.email",
+profile = "https://www.googleapis.com/auth/userinfo.profile";
+
+
+
+
+
