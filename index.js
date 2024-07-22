3ed3c762228a09223dca6f23aae5faaa526db99c
JosuePlataV <144969668+JosuePlataV@users.noreply.github.com>
2024-07-21 21:09:14 -0500

    no se que hago con mi vida

--- index.js
+++ index.js
@@ -1,32 +1,11 @@
-let Container = document.querySelector("#Container"),
-url = "https://people.googleapis.com/$discovery/rest?version=v1",
-email = "https://www.googleapis.com/auth/userinfo.email",
-profile = "https://www.googleapis.com/auth/userinfo.profile",
+import { GoogleLogin } from 'react-google-login';
+let Container = document.querySelector(".g-signin2"),
 clientid = "164538105610-d0fclos8th1prrb4fem2vjllsj3cg49o.apps.googleusercontent.com",
-login = document.querySelector("#login");
+secret = "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"
 
-var googleUser = {};
-        var startApp = function() {
-            gapi.load('auth2', function(){
-                auth2 = gapi.auth2.init({
-                    clientid: 'clientid',
-                    cookiepolicy: 'single_host_origin',
-                    scope: 'https://www.googleapis.com/auth/gmail.readonly'
-                });
-                attachSignin(document.getElementById('login'));
-            });
-        };
+function init() {
+    gapi.load('auth2', function() {
+        client_id: clientid
+    });
+  }
 
-        function attachSignin(element) {
-            auth2.attachClickHandler(element, {},
-                function(googleUser) {
-                    var profile = googleUser.getBasicProfile();
-                    var id_token = googleUser.getAuthResponse().id_token;
-                    // Redirigir a la segunda pantalla con el token de ID
-                    window.location.href = "/profile.html?id_token=" + id_token;
-                }, function(error) {
-                    console.log(JSON.stringify(error, undefined, 2));
-                });
-        }
-
-        startApp();
