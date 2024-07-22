c452d166e6cc95f19fca68740da5f54f5f8eaee6
JosuePlataV <144969668+JosuePlataV@users.noreply.github.com>
2024-07-22 01:00:41 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 function onSignIn(googleUser) {
     var profile = googleUser.getBasicProfile();
-    $("#image").attr(src,profile.getImageUrl());
+    $("#image").attr('src',profile.getImageUrl());
     $("#name").text(profile.getName());
     $("#email").text(profile.getEmail());
     $(".data").css("display", "block");
