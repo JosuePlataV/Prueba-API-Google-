9fed03576ff71d2a1b31bc9e88dfae65b9052f4a
JosuePlataV <144969668+JosuePlataV@users.noreply.github.com>
2024-07-21 23:28:56 -0500

    idkwtdwmla

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 function onSignIn(googleUser) {
     var profile = googleUser.getBasicProfile();
     function onSignIn(googleUser) {
-        document.getElementById('name').textContent = profile.getName();
-        document.getElementById('email').textContent = profile.getEmail();
-        document.getElementById('image').src = profile.getImageUrl();
-        $(".data").css("display", "block");
-        $(".g-signin2").css("display", "none");
+        document.getElementById$('name').textContent = profile.getName();
+        document.getElementById$('email').textContent = profile.getEmail();
+        document.getElementById$('image').src = profile.getImageUrl();
+        document.getElementsByClassName$(".data").css("display", "block");
+        document.getElementsByClassName$(".g-signin2").css("display", "none");
     }
 }
  
