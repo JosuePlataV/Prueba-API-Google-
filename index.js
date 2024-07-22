160cca093e6fb6f5dcfa5a6303d436512a47c7cb
JosuePlataV <144969668+JosuePlataV@users.noreply.github.com>
2024-07-22 02:14:55 -0500

    d

--- index.js
+++ index.js
@@ -1,15 +1,17 @@
 function onSignIn(googleUser) {
     var profile = googleUser.getBasicProfile();
-
-    // Actualizar los elementos en el HTML con los datos del perfil
-    document.getElementById('id').textContent = profile.getId();
-    document.getElementById('name').textContent = profile.getName();
-    document.getElementById('givenName').textContent = profile.getGivenName();
-    document.getElementById('familyName').textContent = profile.getFamilyName();
-    document.getElementById('image').src = profile.getImageUrl();
-    document.getElementById('email').textContent = profile.getEmail();
-
-    // Mostrar los datos del perfil y ocultar el botón de inicio de sesión
-    document.querySelector('.data').style.display = 'block';
-    document.querySelector('.g-signin2').style.display = 'none';
+    $("#image").attr('src', profile.getImageUrl());
+    $("#name").text(profile.getName());
+    $("#email").text(profile.getEmail());
+    $(".data").css("display", "block");
+    $(".g-signin2").css("display", "none");
+}
+ 
+function signOut(){
+    var auth2 = gapi.auth2.getAuthInstance();
+    auth2.signOut().then(function(){
+        alert("Has cerrado sesión correctamente");
+        $(".g-signin2").css("display", "block");
+        $(".data").css("display", "none");
+    })
 }
