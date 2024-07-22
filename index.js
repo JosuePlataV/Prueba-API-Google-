5fd13c9b590d283e49e6e19accca22396e8bad2e
JosuePlataV <144969668+JosuePlataV@users.noreply.github.com>
2024-07-22 13:56:13 -0500

    hope?

--- index.js
+++ index.js
@@ -1,17 +1,23 @@
-function onSignIn(googleUser) {
-    var profile = googleUser.getBasicProfile();
-    $("#image").attr('src', profile.getImageUrl());
-    $("#name").text(profile.getName());
-    $("#email").text(profile.getEmail());
-    $(".data").css("display", "block");
-    $(".g-signin2").css("display", "none");
+// Configura la función de callback que se llama cuando el usuario inicia sesión
+function handleCredentialResponse(response) {
+    const responsePayload = decodeJwtResponse(response.credential);
+
+    // Usar jQuery para manipular el DOM
+    $("#image").attr("src", responsePayload.picture);
+    $("#name").text(responsePayload.name);
+    $("#email").text(responsePayload.email);
+    $(".data").show(); // Mostrar los datos del usuario
+    $(".g_id_signin").hide(); // Ocultar el botón de inicio de sesión
 }
- 
-function signOut(){
-    var auth2 = gapi.auth2.getAuthInstance();
-    auth2.signOut().then(function(){
-        alert("Has cerrado sesión correctamente");
-        $(".g-signin2").css("display", "block");
-        $(".data").css("display", "none");
-    })
+
+// Decodificar el token JWT para obtener la información del usuario
+function decodeJwtResponse(token) {
+    var base64Url = token.split('.')[1];
+    var base64 = base64Url.replace(/-/g, '+').replace(/_/g, '/');
+    var jsonPayload = decodeURIComponent(atob(base64).split('').map(function(c) {
+        return '%' + ('00' + c.charCodeAt(0).toString(16)).slice(-2);
+    }).join(''));
+
+    return JSON.parse(jsonPayload);
 }
+
