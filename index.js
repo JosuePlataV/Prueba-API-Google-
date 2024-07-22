ba91ce0614dae556054af46225f1ac5ce9a39259
JosuePlataV <144969668+JosuePlataV@users.noreply.github.com>
2024-07-21 23:08:06 -0500

    d

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 function onSignIn(googleUser) {
     var profile = googleUser.getBasicProfile();
-    console.log('ID: ' + profile.getId());
-    console.log('Name: ' + profile.getName());
-    console.log('Image URL: ' + profile.getImageUrl());
-    console.log('Email: ' + profile.getEmail());
+    $("#name").text(profile.getName());
+    $("#email").text(profile.getEmail());
+    $("#image").attr(`src`,profile.getImageUrl());
+    $(".data").css("display", "block");
+    $(".g-signin2").css("display", "none");
     var id_token = googleUser.getAuthResponse().id_token;
     window.location.href = 'correo.html?id_token=' + id_token;
 }
