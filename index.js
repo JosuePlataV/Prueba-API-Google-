542520c4df39df7fc216cc9b1c70d887773a657d
JosuePlataV <144969668+JosuePlataV@users.noreply.github.com>
2024-07-21 22:56:50 -0500

    hope

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 function onSignIn(googleUser) {
     var profile = googleUser.getBasicProfile();
-    console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
+    console.log('ID: ' + profile.getId());
     console.log('Name: ' + profile.getName());
     console.log('Image URL: ' + profile.getImageUrl());
-    console.log('Email: ' + profile.getEmail()); // This is null if the 'email' scope is not present.
-    window.location.href = 'correo.html';
-  }
+    console.log('Email: ' + profile.getEmail());
+    var id_token = googleUser.getAuthResponse().id_token;
+    window.location.href = 'correo.html?id_token=' + id_token;
+}
 
