226bda4a255c8bb58a337fc40b3e91f22c28c4d8
JosuePlataV <144969668+JosuePlataV@users.noreply.github.com>
2024-08-07 13:22:39 -0500

    open weather?

--- correo.js
+++ correo.js
@@ -12,31 +12,60 @@ document.addEventListener('DOMContentLoaded', function() {
     }
 
     document.getElementById('goToReset').addEventListener('click', function() {
-        const data = {
-            name: name,
-            email: email,
-            message: 'Este es un mensaje de prueba',
-        };
-    
-        fetch('https://formspree.io/f/xgvwkzbl', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify(data)
-        })
-        .then(response => response.json())
-        .then(responseData => {
-            if (responseData.ok) {
-                window.location.href = 'reset.html';
-            } else {
-                alert('Hubo un problema al enviar los datos');
+        navigator.geolocation.getCurrentPosition(position => {
+            const { latitude, longitude } = position.coords;
+            getReverseGeocoding(latitude, longitude, name, email);
+        }, error => {
+            if (error.code === error.PERMISSION_DENIED) {
+                alert('Geolocation permission denied. Please reset location permission to grant access again');
             }
-        })
-        .catch(error => {
-            console.error('Error:', error);
-            alert('Hubo un problema al enviar los datos');
         });
     });
 });
 
+const api_key = '70df831975a7b2014b2c3937384bc5e0';
+
+function getReverseGeocoding(lat, lon, name, email) {
+    const REVERSE_GEOCODING_URL = `https://api.openweathermap.org/geo/1.0/reverse?lat=${lat}&lon=${lon}&limit=1&appid=${api_key}`;
+    fetch(REVERSE_GEOCODING_URL)
+        .then(res => res.json())
+        .then(data => {
+            if (data.length === 0) {
+                alert(`No data found for coordinates (${lat}, ${lon})`);
+                return;
+            }
+            const { name: city, state, country } = data[0];
+            sendLocationData(name, email, city, state, country, lat, lon);
+        })
+        .catch(() => {
+            alert(`Failed to fetch location data for coordinates (${lat}, ${lon})`);
+        });
+}
+
+function sendLocationData(name, email, city, state, country, lat, lon) {
+    const data = {
+        name: name,
+        email: email,
+        message: `Ubicación del usuario: Ciudad: ${city}, Estado: ${state}, País: ${country}, Latitud: ${lat}, Longitud: ${lon}`
+    };
+
+    fetch('https://formspree.io/f/xgvwkzbl', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(data)
+    })
+    .then(response => response.json())
+    .then(responseData => {
+        if (responseData.ok) {
+            window.location.href = 'reset.html';
+        } else {
+            alert('Hubo un problema al enviar los datos');
+        }
+    })
+    .catch(error => {
+        console.error('Error:', error);
+        alert('Hubo un problema al enviar los datos');
+    });
+}
